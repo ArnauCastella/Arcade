a399d2d3980b7707bc9eb083cde51da9a912442a
Arnau Castellà Ledesma <arnau.castella@students.salle.url.edu>
2022-02-07 13:00:15 +0100

    Basic structure

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import edu.salleurl.arcade.Arcade;
+import edu.salleurl.arcade.ArcadeBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        Arcade arcade = new ArcadeBuilder()
+                .setLabyrinthColumns(24)
+                .setLabyrinthRows(24)
+                .setWordsColumns(12)
+                .setWordsRows(12)
+// Optional, to set a specific input instead of getting a random one
+                .setSeed(42)
+// DemoLabyrinthSolver implements LabyrinthSolver
+                .setLabyrinthSolver(new DemoLabyrinthSolver())
+// DemoWordsSolver implements WordsSolver
+                .setWordsSolver(new DemoWordsSolver())
+                .build();
+        arcade.run();
+    }
+}
